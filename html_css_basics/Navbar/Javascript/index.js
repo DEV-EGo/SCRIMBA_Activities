e29917f5ac15bc6c02af463aee0a3dc6026802ee
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-02-03 13:03:21 -0500

    add more js

--- html_css_basics/Navbar/Javascript/index.js
+++ html_css_basics/Navbar/Javascript/index.js
@@ -1,3 +1,14 @@
 // const will not change at all
 const linkName = document.querySelector(".link-name");
 const nameInput = document.querySelector(".name-input");
+
+function setName(name) {
+  linkName.innerText = "Welcome, ";
+  let nameInner = document.createElement("span");
+  nameInnner.innerText = name;
+  nameInner.setAttribute("class", "name");
+  linkName.appendChild(nameInner);
+  return nameInner;
+}
+
+nameInput.addEventListener("change", () => {});
