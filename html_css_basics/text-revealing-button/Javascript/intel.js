57e9af207ecc4cf3be9b22cc024c6db7da77902b
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-02-05 20:11:29 -0500

    new lay out for form

--- html_css_basics/text-revealing-button/Javascript/intel.js
+++ html_css_basics/text-revealing-button/Javascript/intel.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector(".btn"); //choosing the button
 const text = document.querySelector(".text"); // choosing the text
-let shown = true; //will tell us wether the text is showing or not on the page
+let sho.badwn = true; //will tell us wether the text is showing or not on the page
 
 function toggleText() {
   //creating "if" statement that will use "shown" variable to make changes
