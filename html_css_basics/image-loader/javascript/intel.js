d55f8ebfd2e1cdb6689871ed5a59eed49be6c57a
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-02-08 12:43:25 -0500

    new const

---
+++ html_css_basics/image-loader/javascript/intel.js
@@ -0,0 +1,4 @@
+const urlInput = document.querySelector("input[name='url']");
+const urlInput = document.querySelector("input[name='width']");
+const colorInput = document.querySelector("input[name='color']");
+const image = document.querySelector(".image");
