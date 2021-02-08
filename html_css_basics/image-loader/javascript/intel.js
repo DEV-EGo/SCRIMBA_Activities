8d9f375158e1a6d6ee8ededf2b576d3ed7632604
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-02-08 12:53:08 -0500

    add

--- html_css_basics/image-loader/javascript/intel.js
+++ html_css_basics/image-loader/javascript/intel.js
@@ -2,3 +2,23 @@ const urlInput = document.querySelector("input[name='url']");
 const urlInput = document.querySelector("input[name='width']");
 const colorInput = document.querySelector("input[name='color']");
 const image = document.querySelector(".image");
+
+let url = "";
+let width = 10;
+let color = "#000000";
+
+function hideImageOnLoad() {
+  image.style.display = "none";
+}
+
+function setImage(url, width, color) {
+  image.style.display = "block";
+  image.style.borderStyle = "solid";
+  image.style.backgroundImage = "url(" + url + ")";
+  image.style.borderwidth = width + "px";
+  image.style.borderColor = color;
+}
+
+urlInput.addEventListener("change", () => {
+  setImage(url, width, color);
+});
